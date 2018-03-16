16c8feea900577e9a2011288810e756c871b912d
fockness <1162496378@qq.com>
2018-03-16 15:19:52 +0800

    测试

---
+++ src/main/java/com/test/controller/BookContrller.java
@@ -0,0 +1,72 @@
+package com.test.controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.entity.User;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@RequestMapping(value = "/users")
+public class BookContrller {
+
+    Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
+
+    @ApiOperation(value="获取图书列表", notes="获取图书列表")
+    @RequestMapping(value={""}, method= RequestMethod.GET)
+    public List<User> getBook() {
+        List<User> book = new ArrayList<>(users.values());
+        return book;
+    }
+
+    @ApiOperation(value="创建图书", notes="创建图书")
+    @ApiImplicitParam(name = "book", value = "图书详细实体", required = true, dataType = "Book")
+    @RequestMapping(value="", method=RequestMethod.POST)
+    public String postBook(@RequestBody User user) {
+        return "success";
+    }
+    @ApiOperation(value="获图书细信息", notes="根据url的id来获取详细信息")
+    @ApiImplicitParam(name = "id", value = "ID", required = true, dataType = "Long",paramType = "path")
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        return users.get(id);
+    }
+
+    @ApiOperation(value="更新信息", notes="根据url的id来指定更新图书信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "图书ID", required = true, dataType = "Long",paramType = "path"),
+            @ApiImplicitParam(name = "user", value = "图书实体book", required = true, dataType = "User")
+    })
+    @RequestMapping(value="/{id}", method= RequestMethod.PUT)
+    public String putUser(@PathVariable Long id, @RequestBody User user) {
+        User u = users.get(id);
+        u.setUsername(user.getUsername());
+        return "success";
+    }
+    @ApiOperation(value="删除图书", notes="根据url的id来指定删除图书")
+    @ApiImplicitParam(name = "id", value = "图书ID", required = true, dataType = "Long",paramType = "path")
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id) {
+        users.remove(id);
+        return "success";
+    }
+
+    @ApiIgnore//使用该注解忽略这个API
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String  jsonTest() {
+        return " hi you!";
+    }
+}
